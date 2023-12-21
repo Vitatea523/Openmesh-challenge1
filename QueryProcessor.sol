2eec1a2c4b3a042e42095e0947ebe25df64ad8c6
Zoey <zoey@teletrack.com>
2023-12-21 22:22:53 +1100

    modify QueryProcessor

--- QueryProcessor.sol
+++ QueryProcessor.sol
@@ -13,47 +13,47 @@ contract QueryProcessor {
         // Add more operation types as needed
     }
 
-    // Define an event that notifies the query results, and emit statements are typically used to trigger the event.
-    // An event is a communication mechanism in a contract,
-    // Can be used to notify external observers (such as front-end applications) of some important contract state change.
-    event QueryResult(uint indexed queryNum, string result);    
+    event QueryResult(uint indexed queryNum, bytes result);    
+    
     // User submitted query structure
     struct QueryOperation {
         OperationType operationType;
-        bytes[] parameters; // Use bytes instead of string
+        bytes parameters; // Use bytes instead of string
     }
 
     uint256 public queryNums;
-    string public result;
+    bytes public result;
     IDataQuery public dataQuery;
     address public owner;
 
     mapping(uint256 => QueryOperation) public queryOperations;
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Permission denied");
-        _; // Continue with the function execution if the modifier check passes
+    modifier onlyEther() {
+        // Check that msg.sender is a valid Ethereum address
+        require(msg.sender == address(msg.sender), "Invalid Ethereum address");
+        _;
     }
+
 
     constructor(address _dataQueryAddress) {
         dataQuery = IDataQuery(_dataQueryAddress);
         owner = msg.sender;
     }
 
-    function submitQueries(QueryOperation[] memory _queries) external onlyOwner {
-        require( _queries.length > 0, "No queies");
+    function submitQueries(bool isOnChain, QueryOperation[] memory _queries) external onlyEther {
+        require( _queries.length > 0, "No queries");
         for (uint256 i = 0; i < _queries.length; i++) {
             queryNums++;
             queryOperations[queryNums] = _queries[i];
-            executeSingleQuery(queryOperations[queryNums]);
+            executeSingleQuery(isOnChain, queryOperations[queryNums]);
         }
         emit QueryResult(queryNums, result);
         return;
     }
 
-    function executeSingleQuery(QueryOperation memory _query) internal {
+    function executeSingleQuery(bool isOnChain, QueryOperation memory _query) internal {
         if (_query.operationType == OperationType.GetData) {
-            getData(_query.parameters);
+            getData(isOnChain,_query.parameters);
         } else if (_query.operationType == OperationType.Select) {
             select(_query.parameters);
         } else if (_query.operationType == OperationType.FilterBy) {
@@ -62,12 +62,11 @@ contract QueryProcessor {
     }
 
     // A function to get data
-    function getData(bytes[] memory _parameter) internal {
+    function getData(bool isOnChain, bytes memory dataName) internal {
         
         // TODO: Simulate calls to other contracts to get data
-        string memory _result = dataQuery.getData(_parameter);
+        bytes memory _result = dataQuery.getData(isOnChain, string(dataName));
         require(bytes(_result).length > 0, "Data cannot be empty");
-        require(_result.isValidJson(), "Invalid JSON data");
 
         // Process the obtained data, which can be performed according to the actual situation
         processResult(_result);
@@ -75,22 +74,22 @@ contract QueryProcessor {
     }
 
     // select fields
-    function select(string[] memory fields) internal {
+    function select(bytes memory fields) internal {
         // Simulates the selection of data from a specified field in a data set
-        string memory _result = selectFields(fields);
+        bytes memory _result = selectFields(fields);
         
         processResult(_result);
     }
 
     // A function of the filterBy condition
-    function filterBy(bytes[] memory conditions) internal {
-        string memory _result = applyFilterConditions(conditions);
+    function filterBy(bytes memory conditions) internal {
+        bytes memory _result = applyFilterConditions(conditions);
 
         processResult(_result);
 
     }
     // A function that processes the results of a query
-    function processResult(string memory _result) internal {
+    function processResult(bytes memory _result) internal {
         result = _result;
         // Add the actual processing logic for the query results here
         // Parsing JSON data
@@ -98,14 +97,14 @@ contract QueryProcessor {
     }
 
     // Auxiliary function that simulates selected field data
-    function selectFields(bytes[] memory fields) internal pure returns (string memory) {
+    function selectFields(bytes memory fields) internal pure returns (bytes memory) {
         // The actual logic can be modified to suit your needs
         // TODO
         return'{"FilteredField": "FilteredData"}';
     }
 
     // Auxiliary functions that simulate filtering data according to conditions
-    function applyWhereConditions(bytes[] memory conditions) internal pure returns (string memory) {
+    function applyFilterConditions(bytes memory conditions) internal pure returns (bytes memory) {
         // TODO
         return'{"FilteredField": "FilteredData"}';
     }
